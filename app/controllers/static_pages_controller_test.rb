1c99464d574f57b48c477bec2300f3690d71bee4
Nicola <nicolaninni97@gmail.com>
2019-11-09 20:07:28 +0100

    songkick-gem
    Repull

--- app/controllers/static_pages_controller_test.rb
+++ app/controllers/static_pages_controller_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
 
+  def concerts
+    remote = Songkickr::Remote.new ENV["SONGKICK_API_KEY"]
+    @results = remote.events(location: "clientip").results
+  end
+
   test "should get home" do
     get :home
     assert_response :success
