85c2d0930903b26746c3ca0b12df57af02e3d4c8
Daniele <daniele.michelori@gmail.com>
2019-11-14 18:56:26 +0100

    Chatroom 0.3
    Risolto l'errore App is not defined
    Sistemare l'estetica della chat inserendo i msg, l'immagine e il nome profilo nel box "Chatroom" dopo essersi loggati

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages2/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root             'static_pages#homepage'
 
-  #
   mount ActionCable.server => '/cable'
 
   # rotte _header
