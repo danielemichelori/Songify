ff5e6f626827ba6e1aec884ff296600a624fd809
Daniele <daniele.michelori@gmail.com>
2019-11-20 18:23:39 +0100

    Profile 0.2

--- db/migrate/20191112151810_update_users.rb
+++ db/migrate/20191112151810_update_users.rb
@@ -5,8 +5,9 @@ class UpdateUsers < ActiveRecord::Migration[5.2]
       add_column :users, :uid, :string, limit: 500, null: false, default: 'user-id'
       add_column :users, :first_name, :string, limit: 20, null: false,  unique: true, default: 'name'
       add_column :users, :last_name, :string, limit: 20, null: false,  unique: true, default: 'surname'
-      add_column :users, :date_of_birth, :date, unique: true, default: 'GG-MM-AA'
-      add_column :users, :gender, :integer, default: 'others'
+      add_column :users, :date_of_birth, :date, unique: true, default: '2014-12-01T01:29:18'
+      add_column :users, :gender, :string, default: 'others'
+      add_column :users, :email, :string, default: 'email@gmail.com'
       add_column :users, :twittername, :string, limit:20, null: false, unique: true, default: 'twitter-name'
     end
 end
