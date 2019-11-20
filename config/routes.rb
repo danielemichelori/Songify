e6450b2b539cd1c78947c3b561e2c23376eeadab
Daniele <daniele.michelori@gmail.com>
2019-11-20 11:44:01 +0100

    Cambiamenti
    Rails

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
   root             'static_pages#homepage'
 
-  mount ActionCable.server => '/cable'
-
   devise_for :users , controllers: {
     session: 'users/session',
     registration: 'users/registration',
@@ -12,19 +10,18 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks",
     confirmations: 'users/confirmations'}
 
-  get 'profile/:id'    => 'users#show', as: 'user'
-  resources :tweets
-  resources :users, :only =>[:show, :index]
-
   # rotte _header
   get 'homepage'       => 'static_pages#homepage'  #rotta homepage
   get 'concerts'       => 'static_pages#concerts'  #rotta concerti
   get 'artists'        => 'static_pages#artists'   #rotta artisti
   get 'tweets'         => 'static_pages#tweets'    #rotta tweets
-  get 'signin'         => 'static_pages#signin'    #rotta signin
   get 'chatroom'       => 'static_pages#chatroom'  #rotta chatroom
-  
   # rotte _footer
   get 'about'          => 'static_pages#about'     #rotta informazioni
   get 'contact'        => 'static_pages#contact'   #rotta contatti
+
+  mount ActionCable.server => '/cable'
+
+  resources :tweets
+  resources :users, :only =>[:show, :index]
 end
