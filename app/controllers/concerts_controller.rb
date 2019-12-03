4a510660e36e88f2cb940ea38fe70c98af720897
Nicola <nicolaninni97@gmail.com>
2019-12-03 16:57:52 +0100

    Concerts 0.2
    Concerts details upgrade

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,5 +1,5 @@
 class ConcertsController < ApplicationController
-    
+
     remote = Songkickr::Remote.new ENV["SONGKICK_API_KEY"]
     @@results = remote.events(location: "clientip").results
     
@@ -19,6 +19,20 @@ class ConcertsController < ApplicationController
                 return event
             end
         end
-        return "No event found"
-    end 
+        render_not_found
+    end
+
+    private
+    def render_not_found
+        render :file => "#{Rails.root}/public/404.html",  :status => 404
+    end
+
+    private
+    def get_artist_bio(artist)
+        lastfm = Lastfm.new(ENV["LASTFM_API_KEY"], ENV["LASTFM_API_SECRET"])
+        token = lastfm.auth.get_token 
+        lastfm.session = lastfm.auth.get_session(token: token)['key']
+
+        return lastfm.artist.getInfo(artist: artist)
+    end
 end
