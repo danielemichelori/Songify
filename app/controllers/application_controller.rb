b6b3004e0a26bd92d3a559215b7bce53567fadce
Riccardo Salvalaggio <salvalaggioriccardo@gmail.com>
2020-03-18 16:51:03 +0100

    Edit profile (Twitter)
    Users from twitter can edit their profile.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,9 @@ class ApplicationController < ActionController::Base
                                       :first_name, :last_name])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :date_of_birth, :gender,
-                                      :first_name, :last_name, :twittername, :created_at, :profile_picture_url, :avatar, :avatar_cache, :remove_avatar, :ban, :bio])
+                                      :first_name, :last_name, :twittername, :created_at,:location, :profile_picture_url, :avatar, :avatar_cache, :remove_avatar, :ban, :bio])
     
   end
-
-
 
   def favorite_text
     return @favorite_exists ? "UnFavorite" : "Favorite"
