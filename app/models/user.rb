7e9fe2b2817a77e7562bd3ee5d7ffa0de0c1d360
Riccardo Salvalaggio <salvalaggioriccardo@gmail.com>
2020-03-18 16:51:03 +0100

    Edit profile (Twitter)
    Users from twitter can edit their profile.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
   has_many :comments, dependent: :destroy
   has_many :reports, dependent: :destroy
+  has_many :valuations, dependent: :destroy
   acts_as_voter
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -25,7 +26,7 @@ class User < ApplicationRecord
       user.username = auth.info.nickname
       user.first_name  = auth.info.name              # assuming the user model has a name
       user.email = auth.info.email            # since we are using twitter, which hasn't got any email, we'll just comment this
-      user.password = Devise.friendly_token[0, 20]
+      user.password = 123456
       user.provider = auth.provider
       user.profile_image_url = auth[:extra][:raw_info][:profile_image_url]
       # If you are using confirmable and the provider(s) you use validate emails,
