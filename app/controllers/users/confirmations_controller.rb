0ce9082370fc5bc18130046352dfc2aeb666ab77
Riccardo Salvalaggio <salvalaggioriccardo@gmail.com>
2020-03-20 11:32:26 +0100

    Confirmation e-mail 0.1
    Now, on sign-up, songify send an e-mail to confirm your account.

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -13,7 +13,15 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
 
   # GET /resource/confirmation?confirmation_token=abcdef
    def show
-    super
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      respond_with_navigational(resource){ redirect_to user_path(resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
    end
 
   # protected
@@ -24,7 +32,7 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # end
 
   # The path used after confirmation.
-  # def after_confirmation_path_for(resource_name, resource)
-  #   super(resource_name, resource)
-  # end
+   def after_confirmation_path_for(resource_name, resource)
+    redirect_to user_path(resource)
+   end
 end
